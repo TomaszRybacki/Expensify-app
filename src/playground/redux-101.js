f670be34e0a89ad0c9235e2fe85a48c7bc73216a
Tomasz89 <TomaszRybacki1989@gmail.com>
2019-01-01 13:59:01 +0100

    "store subscribe" added

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -4,12 +4,14 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       };
     case 'RESET':
       return {
@@ -19,25 +21,26 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
+});
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
 });
 
 store.dispatch({
   type: 'DECREMENT'
 });
 
-store.dispatch({
-  type: 'DECREMENT'
-});
-
-console.log(store.getState());
+unsubscribe();
 
 store.dispatch({
   type: 'RESET'
 });
-
-console.log(store.getState());
