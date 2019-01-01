a1ea1cba05cf446463ae2cd4045096ec2dee772a
Tomasz89 <TomaszRybacki1989@gmail.com>
2019-01-01 12:07:58 +0100

    "createStore" added

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ const Header = () => (
 
     <NavLink to="/" activeClassName="is-active" exact={true}> Dashboard </NavLink>
     <NavLink to="/create" activeClassName="is-active"> Create Expense </NavLink>
-    <NavLink to="/edit" activeClassName="is-active"> Edit Expense </NavLink>
     <NavLink to="/help" activeClassName="is-active"> Help </NavLink>
   </header>
 );
---
+++ src/playground/redux-101.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+
+
+const store = createStore((state = { count: 0 }) => {
+  return state;
+});
+
+
+console.log(store.getState());
