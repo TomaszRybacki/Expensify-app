cde1285569b8879742e598dd578fa3a6544389dd
Tomasz89 <TomaszRybacki1989@gmail.com>
2019-01-06 19:01:54 +0100

    "remove expense" button added

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setTextFilter } from '../actions/filters';
 
-const ExpenseListFilters = (props) => (
+const ExpenseListFilters = props => (
   <div>
-    <input type="text" value={props.filters.text} onChange={(e) => {
-      props.dispatch(setTextFilter(e.target.value));
-    }}/>
+    <input
+      type="text"
+      value={props.filters.text}
+      onChange={(e) => { props.dispatch(setTextFilter(e.target.value)); }}
+    />
   </div>
 );
 
-
-const mapStateToProps = (state) => {
-  return {
+const mapStateToProps = state => (
+  {
     filters: state.filters
-  };
-};
+  }
+);
 
 export default connect(mapStateToProps)(ExpenseListFilters);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-export default ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({
+  dispatch, id, description, amount, createdAt
+}) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
+    <button onClick={() => {
+      dispatch(removeExpense({ id }));
+    }}
+    >Remove
+    </button>
   </div>
 );
+
+export default connect()(ExpenseListItem);
